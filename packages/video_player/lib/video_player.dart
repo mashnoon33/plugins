9c1e43c2e28014ea2527f2840e9fda608fe8cb24
mashNY <mashnoon33@gmail.com>
2019-05-05 15:21:37 -0500

    Removed didChangeAppLifecycleState to enable background play

--- packages/video_player/lib/video_player.dart
+++ packages/video_player/lib/video_player.dart
@@ -431,21 +431,21 @@ class _VideoAppLifeCycleObserver extends Object with WidgetsBindingObserver {
     WidgetsBinding.instance.addObserver(this);
   }
 
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    switch (state) {
-      case AppLifecycleState.paused:
-        _wasPlayingBeforePause = _controller.value.isPlaying;
-        _controller.pause();
-        break;
-      case AppLifecycleState.resumed:
-        if (_wasPlayingBeforePause) {
-          _controller.play();
-        }
-        break;
-      default:
-    }
-  }
+  // @override
+  // void didChangeAppLifecycleState(AppLifecycleState state) {
+  //   switch (state) {
+  //     case AppLifecycleState.paused:
+  //       _wasPlayingBeforePause = _controller.value.isPlaying;
+  //       _controller.pause();
+  //       break;
+  //     case AppLifecycleState.resumed:
+  //       if (_wasPlayingBeforePause) {
+  //         _controller.play();
+  //       }
+  //       break;
+  //     default:
+  //   }
+  // }
 
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
